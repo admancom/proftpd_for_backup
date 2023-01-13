d83a346362af0a48138aed421595b1b5982bcb49
Pavel <allyourbase2012@rambler.ru>
2023-01-13 20:56:10 +0300

    Initial comment 4

--- start_proftpd.sh
+++ start_proftpd.sh
@@ -2,7 +2,7 @@
 app="proftpd"
 docker build -t ${app} .
 
-docker run -d -it -p 0.0.0.0:21:21 -p 0.0.0.0:30091-30100:30091-30100 --restart unless-stopped --name=${app} --mount type=bind,source=/home,target=/home/data ${app}
+docker run -d -it -p 0.0.0.0:21:21 -p 0.0.0.0:30091-30100:30091-30100 --restart unless-stopped --name=${app} --mount type=bind,source=/home/data,target=/home/data ${app}
 
 
 
